8a866c9642714c671c840af274d2ef14eb58021f
“saschakala” <mxsaschakala@gmail.com>
2020-12-03 15:43:53 -0500

    commit tree 6d62ccd265842167ca00e335398474bfade02b63

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(payCard) {
+    return {
+        firstName: payCard[0],
+        familyName: payCard[1],
+        title: payCard[2],
+        payPerHour: payCard[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+}
